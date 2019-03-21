f183a192956b59ccefe16da67718330f9f360188
DowgalOlya <dowgalolya@gmail.com>
2019-03-21 16:39:13 +0200

    Audio focus feature done.

--- app/src/main/java/com/example/android/musicplayerdemo/stateMachine/PlayerManager.kt
+++ app/src/main/java/com/example/android/musicplayerdemo/stateMachine/PlayerManager.kt
@@ -4,24 +4,27 @@ import android.app.PendingIntent
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+import android.media.AudioManager
 import android.media.MediaPlayer
-import android.media.session.PlaybackState.*
+import android.os.PowerManager
 import android.support.v4.media.session.MediaSessionCompat
 import android.support.v4.media.session.PlaybackStateCompat
-import android.util.Log
 import androidx.annotation.MainThread
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.media.session.MediaButtonReceiver
-import com.example.android.musicplayerdemo.di.SingletonHolder
 import com.example.android.musicplayerdemo.entities.TrackMetadata
 import com.example.android.musicplayerdemo.stateMachine.states.IdleState
+import com.example.android.musicplayerdemo.stateMachine.states.PlayingState
 import com.example.android.musicplayerdemo.stateMachine.states.State
 
-class PlayerManager(override val context: Context) : PlayerContext {
+
+@Suppress("DEPRECATION")
+class PlayerManager(override val context: Context) : PlayerContext, AudioManager.OnAudioFocusChangeListener {
 
     private val TAG = "PlayerManager"
-    override var mediaSessionCompat: MediaSessionCompat = MediaSessionCompat(context,TAG)
+    override var mediaSessionCompat: MediaSessionCompat = MediaSessionCompat(context, TAG)
 
     override var mediaPlayer: MediaPlayer = MediaPlayer()
     override val playlist: MutableList<Int> = ArrayList()
@@ -35,6 +38,14 @@ class PlayerManager(override val context: Context) : PlayerContext {
 
     private val _metadata = MutableLiveData<TrackMetadata>()
     val metadata: LiveData<TrackMetadata> = _metadata
+
+    //endregion
+
+    //region AudioManager
+
+    private val audioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+
+
     //endregion
 
     //region mediaSessionCompat
@@ -49,9 +60,17 @@ class PlayerManager(override val context: Context) : PlayerContext {
 
         override fun onPlay() {
             super.onPlay()
-            mediaSessionCompat.isActive = true
-            performAction(Action.Play())
-            setMediaPlaybackState(PlaybackStateCompat.STATE_PLAYING)
+
+            val result = audioManager.requestAudioFocus(
+                this@PlayerManager,
+                AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN
+            )
+
+            if (result == AudioManager.AUDIOFOCUS_GAIN) {
+                mediaSessionCompat.isActive = true
+                performAction(Action.Play())
+                setMediaPlaybackState(PlaybackStateCompat.STATE_PLAYING)
+            }
         }
 
         override fun onSkipToPrevious() {
@@ -79,8 +98,10 @@ class PlayerManager(override val context: Context) : PlayerContext {
         mediaSessionCompat = MediaSessionCompat(context, TAG, mediaButtonReceiver, null)
 
         mediaSessionCompat.setCallback(mediaSessionCallback)
-        mediaSessionCompat.setFlags(MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS
-                or MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS)
+        mediaSessionCompat.setFlags(
+            MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS
+                    or MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS
+        )
 
         val mediaButtonIntent = Intent(Intent.ACTION_MEDIA_BUTTON)
         mediaButtonIntent.setClass(context, MediaButtonReceiver::class.java)
@@ -92,16 +113,20 @@ class PlayerManager(override val context: Context) : PlayerContext {
         val playbackStateBuilder = PlaybackStateCompat.Builder()
         when (state) {
             PlaybackStateCompat.STATE_PLAYING -> {
-                playbackStateBuilder.setActions(PlaybackStateCompat.ACTION_PLAY_PAUSE
-                        or PlaybackStateCompat.ACTION_PAUSE
-                        or PlaybackStateCompat.ACTION_SKIP_TO_NEXT
-                        or PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS)
+                playbackStateBuilder.setActions(
+                    PlaybackStateCompat.ACTION_PLAY_PAUSE
+                            or PlaybackStateCompat.ACTION_PAUSE
+                            or PlaybackStateCompat.ACTION_SKIP_TO_NEXT
+                            or PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS
+                )
             }
             PlaybackStateCompat.STATE_PAUSED -> {
-                playbackStateBuilder.setActions(PlaybackStateCompat.ACTION_PLAY_PAUSE
-                        or PlaybackStateCompat.ACTION_PLAY
-                        or PlaybackStateCompat.ACTION_SKIP_TO_NEXT
-                        or PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS)
+                playbackStateBuilder.setActions(
+                    PlaybackStateCompat.ACTION_PLAY_PAUSE
+                            or PlaybackStateCompat.ACTION_PLAY
+                            or PlaybackStateCompat.ACTION_SKIP_TO_NEXT
+                            or PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS
+                )
             }
         }
 
@@ -109,7 +134,7 @@ class PlayerManager(override val context: Context) : PlayerContext {
         mediaSessionCompat.setPlaybackState(playbackStateBuilder.build())
     }
 
-    //endregion
+//endregion
 
     @MainThread
     private fun performAction(action: Action) {
@@ -121,6 +146,8 @@ class PlayerManager(override val context: Context) : PlayerContext {
         mediaPlayer.setOnCompletionListener {
             mediaSessionCallback.onSkipToNext()
         }
+
+        mediaPlayer.setVolume(1.0f, 1.0f)
 
         mediaSessionCallback.onStop()
         this.playlist.clear()
@@ -140,5 +167,27 @@ class PlayerManager(override val context: Context) : PlayerContext {
     @MainThread
     override fun updateMetadata(metadata: TrackMetadata) {
         _metadata.value = metadata
+    }
+
+    override fun onAudioFocusChange(focusChange: Int) {
+        when (focusChange) {
+            AudioManager.AUDIOFOCUS_LOSS -> {
+                if (currState.value is PlayingState) {
+                    performAction(Action.Pause())
+                }
+            }
+            AudioManager.AUDIOFOCUS_LOSS_TRANSIENT -> {
+                performAction(Action.Pause())
+            }
+            AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK -> {
+                mediaPlayer.setVolume(0.3f, 0.3f)
+            }
+            AudioManager.AUDIOFOCUS_GAIN -> {
+                if (currState.value is IdleState) {
+                    performAction(Action.Pause())
+                }
+                mediaPlayer.setVolume(1.0f, 1.0f)
+            }
+        }
     }
 }
