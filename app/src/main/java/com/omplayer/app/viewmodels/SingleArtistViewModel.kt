fb2ea249845dab6c05eb77f99cb91103dae423a2
DowgalOlya <dowgalolya@gmail.com>
2019-04-08 13:36:30 +0300

    Fix single album tracklist loading.

--- app/src/main/java/com/omplayer/app/viewmodels/SingleArtistViewModel.kt
+++ app/src/main/java/com/omplayer/app/viewmodels/SingleArtistViewModel.kt
@@ -6,9 +6,24 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.omplayer.app.adapters.SingleArtistAdapter
+import com.omplayer.app.db.entities.Track
+import com.omplayer.app.di.SingletonHolder
+import com.omplayer.app.repositories.ArtistRepository
+import com.omplayer.app.repositories.TrackRepository
 import com.omplayer.app.utils.LibraryUtil
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
 class SingleArtistViewModel(application: Application) : AndroidViewModel(application), SingleArtistAdapter.Callback {
+
+    private var parentJob = Job()
+    private val coroutineContext: CoroutineContext
+        get() = parentJob + Dispatchers.IO
+    private val scope = CoroutineScope(coroutineContext)
+    private val db = SingletonHolder.db
+
+    private val trackRepository: TrackRepository = TrackRepository(db.trackDao())
+    private var tracks = ArrayList<Track>()
 
 
     private val _viewLiveData: MutableLiveData<View> = MutableLiveData()
@@ -17,7 +32,15 @@ class SingleArtistViewModel(application: Application) : AndroidViewModel(applica
     val itemAdapter = SingleArtistAdapter(LibraryUtil.selectedArtistAlbumList, this)
 
     override fun openAlbum(albumId: Int, view: View) {
-        _viewLiveData.value = view
+        scope.launch {
+            withContext(coroutineContext) {
+                tracks = trackRepository.getTracksByAlbumId(albumId) as ArrayList<Track>
+                withContext(Dispatchers.Main) {
+                    LibraryUtil.selectedAlbumTracklist = tracks
+                    _viewLiveData.value = view
+                }
+            }
+        }
     }
 
     fun getArtistName(): String {
